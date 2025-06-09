dce591db1280bda122443bce70a2d25fb5235c9f
michailtam <mi.tamvak@gmail.com>
2025-06-09 15:32:59 +0300

    Fixed setup bugs

--- USV-EOSS/app/build.gradle.kts
+++ USV-EOSS/app/build.gradle.kts
@@ -42,11 +42,10 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+    // implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
+    // implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.google.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
